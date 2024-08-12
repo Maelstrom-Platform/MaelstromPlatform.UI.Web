58bcdde226712ac5143c83614d7198d824c5f7d4
Jason Alan Smith <jason.alan.smith@outlook.com>
2024-08-12 10:13:01 -0500

    devops: add url switch between local and dev
    Fixes MP-37.

---
+++ src/MaelstromPlatform.UI/Common/UrlStore.cs
@@ -0,0 +1,8 @@
+﻿namespace MaelstromPlatform.UI.Common
+{
+    public static class UrlStore
+    {
+        public static string API_LOCAL_DEV { get; set; } = "https://localhost:49051";
+        public static string API_AZURE_DEV { get; set; } = "https://api.maelstromplatform-dev.org";
+    }
+}
--- src/MaelstromPlatform.UI/Program.cs
+++ src/MaelstromPlatform.UI/Program.cs
@@ -2,13 +2,25 @@ using MaelstromPlatform.UI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
+using MaelstromPlatform.UI.Common;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var API_URL = string.Empty;
+if (builder.HostEnvironment.IsEnvironment("LOCAL"))
+{
+    API_URL = UrlStore.API_LOCAL_DEV;
+}
+else
+{
+    API_URL = UrlStore.API_AZURE_DEV;
+}
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<DialogService>();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:49051") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(API_URL) });
 
 await builder.Build().RunAsync();
