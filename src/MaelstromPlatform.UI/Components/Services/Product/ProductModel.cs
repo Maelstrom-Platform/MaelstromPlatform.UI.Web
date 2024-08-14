83c791ceb6961640ce7a35c0104dc0c165d775a2
Jason Alan Smith <jason.alan.smith@outlook.com>
2024-08-13 20:14:05 -0500

    feat: add Product UI.Web MVP

---
+++ src/MaelstromPlatform.UI/Components/Services/Product/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MaelstromPlatform.UI.Components.Services.Product
+{
+    public class ProductModel
+    {
+        public Guid SysId { get; set; }
+        public string? Identifier { get; set; }
+
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Mission { get; set; }
+        public string? Vision { get; set; }
+    }
+}
